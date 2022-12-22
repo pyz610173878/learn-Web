c9eca7f8072c0bcdba6e91bed04011e15aa227d7
hacker <610173878@qq.com>
2022-12-23 03:49:25 +0800

    add1

--- test.js
+++ test.js
@@ -9,4 +9,6 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>Hello, World!</h1>');
 });
-
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
